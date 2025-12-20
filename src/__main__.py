3fa28cf7c52c5c7465d8fd8694993baa4577c413
Henrik Lagrosen <helagro@outlook.com>
2025-12-20 12:53:56 +0100

    Add startup indicator

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,4 @@
+from interfaces.actuators.led import get_lamp
 from interfaces.api.config import sync_config
 import server
 import threading
@@ -10,16 +11,24 @@ ran_once = False
 schedule_thread = None
 server_thread = None
 
+
+def start_led_off():
+    led = get_lamp('blue')
+    led.off()
+
+
 if __name__ == '__main__':
 
     if not ran_once:
         ran_once = True
         log("Started")
+        get_lamp('blue').on()
 
         sync_config()
 
         schedule_thread = threading.Thread(target=run_schedule, daemon=True).start()
         server_thread = threading.Thread(target=server.start, daemon=True).start()
+        threading.Timer(5, start_led_off).start()
 
     threading.Event().wait()
 
