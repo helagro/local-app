9279dc0c18495f098db46b19fe956d9eb0fe75f6
Henrik Lagrosen <helagro@outlook.com>
2026-03-31 23:02:42 +0200

    Add cpp server helloworld

--- python_server/interfaces/api/__init__.py
+++ python_server/interfaces/api/__init__.py
@@ -3,4 +3,4 @@ from interfaces.api.server_app import get_routines
 # -------------------------- TESTING ENTRY POINT ------------------------- #
 
 if __name__ == "__main__":
-    print(get_routines("detach"))
+    pass
