d07f447f5b1ecd8619e8a021fd49c9e2ba68ec6e
Henrik Lagrosen <helagro@outlook.com>
2026-04-15 21:20:07 +0200

    Add error handling for cpp_server rest server

--- cpp_server/src/server/server.cpp
+++ cpp_server/src/server/server.cpp
@@ -10,6 +10,14 @@ void start_server() {
     res.set_content("ok", "text/plain");
   });
 
+  svr.set_error_handler([](const auto &req, auto &res) {
+    auto fmt = "<!doctype html><html lang=en><p>Error Status: <span "
+               "style='color:red;'>%d</span></p>";
+    char buf[BUFSIZ];
+    snprintf(buf, sizeof(buf), fmt, res.status);
+    res.set_content(buf, "text/html");
+  });
+
   app_log("Starting server on port 8008...");
   svr.listen("0.0.0.0", 8008);
 }
